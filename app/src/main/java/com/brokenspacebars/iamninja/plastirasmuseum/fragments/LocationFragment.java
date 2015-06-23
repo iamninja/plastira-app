0a9c3e23309da47f3a23f1d0f6ec1dcf084600f8
Vagios Vlachos <bagios234@gmail.com>
2015-06-24 00:13:44 +0300

    Add map fragment

--- app/src/main/java/com/brokenspacebars/iamninja/plastirasmuseum/fragments/LocationFragment.java
+++ app/src/main/java/com/brokenspacebars/iamninja/plastirasmuseum/fragments/LocationFragment.java
@@ -8,13 +8,58 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.brokenspacebars.iamninja.plastirasmuseum.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class LocationFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View V =  inflater.inflate(R.layout.fragment_location, container, false);
+
+        mMapView = (MapView) V.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 39.353;
+        double longitude = 21.749;
+
+        // Create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title(getString(R.string.map_marker_text));
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // Adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(39.353, 21.749)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here
+        return V;
     }
 }
